948d36a81e16ff943a266f59e8d241895ea839d4
Thomas Jaskula <thomasv1000@hotmail.fr>
2016-01-13 18:56:30 +0100

    Adding ggplot of datapoints

--- UnivarianteLinearReg/LinearRegressionGgplot2.fsx
+++ UnivarianteLinearReg/LinearRegressionGgplot2.fsx
@@ -30,6 +30,12 @@ open ggplot
 
 fsi.AddPrinter(fun (synexpr:RDotNet.SymbolicExpression) -> synexpr.Print())
 
+let sizeSettings () =
+    R.theme(namedParams["axis.text", R.element__text(namedParams["size", 12])])
+    ++ R.theme(namedParams["legend.text", R.element__text(namedParams["size", 12])])
+    ++ R.theme(namedParams["axis.title", R.element__text(namedParams["size", 14])])
+    ++ R.theme(namedParams["plot.title", R.element__text(namedParams["size", 18])])
+
 open System.IO
 open System.Globalization
 
@@ -44,11 +50,15 @@ let data = DelimitedReader.Read<double>(dataPath, false, ",", false, dataCulture
 let x = data.Column(0).ToColumnMatrix().ToColumnWiseArray() |> List.ofArray
 let y = data.Column(1).ToColumnMatrix().ToColumnWiseArray() |> List.ofArray
 let df =
-    namedParams ["X", x; "Value" , y]
+    namedParams ["Population", x; "Profit" , y]
     |> R.data_frame
 
 let m = y.Length
 
 // print scatter plot
-G.ggplot(df, G.aes(x="X", y="Value"))
-++ R.geom__point()
+G.ggplot(df, G.aes(x="Population", y="Profit"))
+++ R.xlab("Population of City in 10,000s")
+++ R.ylab("Profit in $10,000s")
+++ R.geom__point(namedParams["shape", box 4; "size", box 2; "colour", box "red"])
+++ R.theme__bw()
+++ sizeSettings()
