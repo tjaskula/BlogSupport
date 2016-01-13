1cd60badcc787359bdd4135d74113954ab6561d1
Thomas Jaskula <thomasv1000@hotmail.fr>
2016-01-13 15:45:58 +0100

    Reworking ggplot2 call in linear regression

--- UnivarianteLinearReg/LinearRegressionGgplot2.fsx
+++ UnivarianteLinearReg/LinearRegressionGgplot2.fsx
@@ -1,33 +1,36 @@
-#r "../packages/MathNet.Numerics/lib/net40/MathNet.Numerics.dll"
-#r "../packages/MathNet.Numerics.FSharp/lib/net40/MathNet.Numerics.FSharp.dll"
-#r "../packages/MathNet.Numerics.Data.Text/lib/net40/MathNet.Numerics.Data.Text.dll"
-#r "../packages/RProvider/lib/net40/RProvider.Runtime.dll"
-#r "../packages/RProvider/lib/net40/RProvider.dll"
-#r "../packages/R.NET.Community/lib/net40/RDotNet.dll"
-#r "../packages/R.NET.Community/lib/net40/RDotNet.NativeLibrary.dll"
-#r "../packages/R.NET.Community.FSharp/lib/net40/RDotNet.FSharp.dll"
-#r "../packages/Deedle/lib/net40/Deedle.dll"
-#r "../packages/Deedle.RPlugin/lib/net40/Deedle.RProvider.Plugin.dll"
-
-#I "../packages/MathNet.Numerics/lib/net40/"
 #I "../packages/Deedle/lib/net40"
 #I "../packages/Deedle.RPlugin/lib/net40"
 #I "../packages/RProvider/lib/net40"
 #I "../packages/R.NET.Community/lib/net40"
 #I "../packages/R.NET.Community.FSharp/lib/net40"
 
+#I "../packages/MathNet.Numerics/lib/net40/"
+#I "../packages/MathNet.Numerics.FSharp/lib/net40/"
+#I "../packages/MathNet.Numerics.Data.Text/lib/net40/"
+
+#r "RProvider.Runtime.dll"
+#r "RProvider.dll"
+#r "RDotNet.dll"
+#r "RDotNet.NativeLibrary.dll"
+#r "RDotNet.FSharp.dll"
+#r "Deedle.dll"
+#r "Deedle.RProvider.Plugin.dll"
+
+#r "MathNet.Numerics.dll"
+#r "MathNet.Numerics.FSharp.dll"
+#r "MathNet.Numerics.Data.Text.dll"
+
+#load "../paket-files/evelinag/ffplot/ggplot.fs"
+
 open Deedle
 open RProvider
 open RProvider.ggplot2
 open RProvider.datasets
-
-#load "../paket-files/evelinag/ffplot/ggplot.fs"
-
 open ggplot
 
-open System
+fsi.AddPrinter(fun (synexpr:RDotNet.SymbolicExpression) -> synexpr.Print())
+
 open System.IO
-open System.Linq
 open System.Globalization
 
 open MathNet.Numerics.LinearAlgebra
@@ -38,9 +41,14 @@ let dataPath = Path.Combine(__SOURCE_DIRECTORY__, "data.csv")
 
 // loads a matrix of 97x2
 let data = DelimitedReader.Read<double>(dataPath, false, ",", false, dataCulture)
-let x = data.Column(0).ToColumnMatrix()
-let y = data.Column(1).ToColumnMatrix()
-let points = Array.zip (x.ToColumnWiseArray()) (y.ToColumnWiseArray()) |> Seq.ofArray
-let m = y.RowCount
+let x = data.Column(0).ToColumnMatrix().ToColumnWiseArray() |> List.ofArray
+let y = data.Column(1).ToColumnMatrix().ToColumnWiseArray() |> List.ofArray
+let df =
+    namedParams ["X", x; "Value" , y]
+    |> R.data_frame
+
+let m = y.Length
 
 // print scatter plot
+G.ggplot(df, G.aes(x="X", y="Value"))
+++ R.geom__line()
