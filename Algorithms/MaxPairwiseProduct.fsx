09f2b465b00eec5b037c2789dfd171bc75261ef1
Tomasz Jaskula <thomasv1000@hotmail.fr>
2016-11-02 00:09:01 +0100

    Working on MaxPairwise F#

--- Algorithms/MaxPairwiseProduct.fsx
+++ Algorithms/MaxPairwiseProduct.fsx
@@ -1,3 +1,14 @@
+// brut force
+let getMaxPariwiseProduct (numbers: int array): int64 =
+    let mutable result = 0L
+    let n = numbers.Length
+    for i in 0..(n - 1) do
+        for j in (i + 1)..(n - 1) do
+            if int64(numbers.[i]) * int64(numbers.[j]) > result then
+                result <- int64(numbers.[i]) * int64(numbers.[j])
+            else ()
+    result
+
 let rec removeFirst pred lst =
     match lst with
     | h::t when pred h -> t
@@ -14,7 +25,9 @@ let findTwoMax l =
             l' 
             |> removeFirst (fun e -> e = m)
             |> twoMaxIter (m :: o)
-    twoMaxIter [] l
+    twoMaxIter [] l 
+    |> List.map (fun e -> int64 e)
+    |> List.reduce (*)
 
 let findTwoMaxFold l =
     l |> List.fold (fun state elem -> 
@@ -23,22 +36,27 @@ let findTwoMaxFold l =
                     elif List.exists (fun e -> e < elem) state then
                         [(List.max state); elem]
                     else state
-                    ) []
+                    ) [] 
+      |> List.map (fun e -> int64 e)
+      |> List.reduce (*)
 
 let l1 = [7; 5; 14; 2; 8; 8; 10; 1; 2; 3]
 let l2 = [7; 5; 14; 2; 15; 15; 10; 1; 2; 3]
 let l3 = [1]
 let l4 = [100000; 90000]
 
-
+l1 |> Array.ofList |> getMaxPariwiseProduct
 l1 |> findTwoMaxFold
 l1 |> findTwoMax
 
-l2 |> findTwoMaxFold
+l2 |> Array.ofList |> getMaxPariwiseProduct
+l2 |> findTwoMaxFold 
 l2 |> findTwoMax
 
-l3 |> findTwoMaxFold
+l3 |> Array.ofList |> getMaxPariwiseProduct
+l3 |> findTwoMaxFold 
 l3 |> findTwoMax
 
+l4 |> Array.ofList |> getMaxPariwiseProduct
 l4 |> findTwoMaxFold
 l4 |> findTwoMax
