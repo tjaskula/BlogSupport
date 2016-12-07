88ff44ea665428e3ae9d18d724beeec2b7892993
Tomasz Jaskula <thomasv1000@hotmail.fr>
2016-12-07 22:11:17 +0100

    Working on PriorityQueue

--- DataStructures/PriorityQueue.fsx
+++ DataStructures/PriorityQueue.fsx
@@ -1,5 +1,71 @@
 namespace FSharpz.Collections
 
+open System
+open Microsoft.FSharp.Core
+
+type PriorityQueue<'T when 'T : comparison>(values: seq<'T>, isDescenfing: bool) =
+    let heap : System.Collections.Generic.List<'T> = System.Collections.Generic.List<'T>(values)
+
+    // by default this is the ascending comparer
+    let cFast = LanguagePrimitives.FastGenericComparer<'T>
+    
+    let mutable size = heap.Count
+
+    let parent i = (i - 1) / 2
+    let leftChild i = 2 * i + 1
+    let rightChild i = 2 * i + 2
+
+    let swap i maxIndex =
+        let temp = heap.[i]
+        heap.[i] <- heap.[maxIndex]
+        heap.[maxIndex] <- temp
+
+    let siftUp i =
+        let mutable indx = i
+        while indx > 0 && heap.[parent indx] < heap.[indx] do
+            swap (parent indx) indx
+            indx <- parent indx
+
+    let rec siftDown i =
+        let l = leftChild i
+        let r = rightChild i
+        let maxIndexLeft = if l < size && heap.[l] > heap.[i] then l else i
+        let maxIndex = if r < size && heap.[r] > heap.[maxIndexLeft] then r else maxIndexLeft
+        if i <> maxIndex then
+            swap i maxIndex
+            siftUp maxIndex
+        else ()
+    
+    let build (unsortedValues: seq<'T>) =
+        for i = size / 2 downto 0 do
+            siftDown i
+    
+    do build heap
+
+    new (values) = PriorityQueue<'T>(values, true)
+
+    member this.IsEmpty = size = 0
+
+    member this.Dequeue() =
+        if this.IsEmpty then raise (new Exception("No more elements to dequeue"))
+        let result = heap.[0]
+        heap.[0] <- heap.[size - 1]
+        // we limit the boundary but the last element stays in memory
+        // we could use heap.Remove but it's O(n) operation so too slow
+        size <- size - 1
+        siftDown 0
+        result
+
+    member this.Enqueue(p: 'T) =
+        if heap.Count = size then
+            heap.Add(p)
+        else
+            heap.[size] <- p
+        size <- size + 1
+        siftUp (size - 1)
+
+
 module PriorityQueue =
-    
-    
+
+    let pMax = new PriorityQueue<int>([|3; 1; 4; 2|])
+    let pMin = new PriorityQueue<int>([|3; 1; 4; 2|], false)
