970131305302bc9bceb46a04685d2baf9400b656
Jaskula Tomasz <thomasv1000@hotmail.fr>
2016-12-14 16:16:59 +0100

    Mutable Graph type

--- DataStructures/PriorityQueue.fsx
+++ DataStructures/PriorityQueue.fsx
@@ -72,18 +72,67 @@ type PriorityQueue<'T when 'T : comparison>(values: seq<'T>, isDescending: bool)
         size <- size + 1
         siftUp (size - 1)
 
+type Edge = { DestinationVertexId: int; Distance: double }
+
+ [<CustomComparison; StructuralEquality>]
 type Vertex = { Id: int; ShortestDistance: double; Edges: Edge list }
-and Edge = { Destination: Vertex; Distance: double }
-type Graph = { Vertices: Vertex list }
+                interface IComparable<Vertex> with
+                        member this.CompareTo other =
+                            compare this.ShortestDistance other.ShortestDistance
+                interface IComparable with
+                    member this.CompareTo(obj: obj) =
+                        match obj with
+                        | :? Vertex -> compare this.ShortestDistance (unbox<Vertex> obj).ShortestDistance
+                        | _ -> invalidArg "obj" "Must be of type Vertex"
 
 let addEdge vertex e = { vertex with Edges = e::vertex.Edges }
 
 type Vertex with
     member this.AddEdge = addEdge this
 
-let addVertex graph v = { graph with Vertices = v::graph.Vertices }
-let getVertices graph = graph.Vertices
+type Graph() =
+    
+    let mutable vertices: Map<int, Vertex> = Map.empty
+    let addVertex (v: Vertex) = vertices <- if vertices.ContainsKey(v.Id) then vertices else vertices.Add(v.Id, v)
+    let setSource vertexId = 
+        vertices <- match vertices.TryFind(vertexId) with
+                     | None -> vertices
+                     | Some(v) -> vertices.Add(v.Id, {v with ShortestDistance = 0.0 })
+        
 
-type Graph with
-    member this.AddVertex = addVertex this
-    member this.GetVertices = getVertices this
+    member this.AddVertex (v: Vertex) = addVertex v; this
+    member this.GetVertices () = vertices |> Map.toList |> List.map snd
+    member this.SetSource vertexId = setSource vertexId; this
+    member this.GetVertex vertexId = vertices.[vertexId]
+
+
+let rawGraph = Map.empty
+                 .Add(1, [(9.0, 3); (7.0, 2); (14.0, 6)])
+                 .Add(2, [(7.0, 1); (10.0, 3); (15.0, 4)])
+                 .Add(3, [(9.0, 1); (2.0, 6); (11.0, 4); (10.0, 2)])
+                 .Add(4, [(15.0, 2); (11.0, 3); (6.0, 5)])
+                 .Add(5, [(6.0, 4); (9.0, 6)])
+                 .Add(6, [(14.0, 1); (2.0, 3); (9.0, 5)])
+
+let makeEdge (distance, destVertexId) =
+    { DestinationVertexId = destVertexId; Distance = distance}
+
+let makeVertex vertexId edges =
+    { Id = vertexId;
+      ShortestDistance = Double.PositiveInfinity;
+      Edges = edges |> List.map makeEdge
+    }
+
+let graph = (rawGraph
+            |> Map.map makeVertex
+            |> Map.fold (fun (graph: Graph) _ v -> graph.AddVertex(v)) (Graph())
+            ).SetSource 1
+
+let pq = PriorityQueue<Vertex>(graph.GetVertices(), false)
+
+while not !pq.IsEmpty do
+    let vertex = pq.Dequeue()
+    for edge in vertex.Edges do
+        let destination = graph.GetVertex edge.DestinationVertexId
+        let newDistance = edge.Distance + vertex.ShortestDistance
+        
