b7c221ae0c64a91e4c464fb7bd72bcb5f03d62a6
Tomasz Jaskula <thomasv1000@hotmail.fr>
2018-12-14 22:30:23 +0100

    Refactoring of the Tree

--- Algorithms/ParallelTreeReduction/TreeReduction/TreeReduction.fsx
+++ Algorithms/ParallelTreeReduction/TreeReduction/TreeReduction.fsx
@@ -1,10 +1,20 @@
 
-type Tree<'a> =
-  | Leaf of value: 'a
-  | Node of left: Tree<'a> * right: Tree<'a>
+type Tree<'T> =
+  | Leaf of 'T
+  | Node of Tree<'T> * Tree<'T>
     
 
 let rec reduce t f =
   match t with
   | Leaf v -> v
-  | Node (l, r) -> f (reduce l f) (reduce r f) 
+  | Node (l, r) -> f (reduce l f) (reduce r f)
+  
+  
+let generateTree height =
+  let rec generateTreeInternal t i =
+    if height = i then t
+    else Node(generateTreeInternal t (i + 1), generateTreeInternal t (i + 1))
+  
+  let nodes = 2 * height - 1  
+  if height <= 0 then failwith "The tree cannot have the height of 0 or less"
+  else Node(generateTreeInternal (Node(Leaf 1, Leaf 2)) 1, generateTreeInternal (Node(Leaf 3, Leaf 4)) 1)
